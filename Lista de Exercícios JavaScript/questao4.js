5c71ada71eb92ee995d7604c632a9cf6e3d2cb50
eduardafred <160436788+eduardafred@users.noreply.github.com>
2024-10-24 02:27:52 +0000

    trabalho finalizado

--- Lista de Exercícios JavaScript/questao4.js
+++ Lista de Exercícios JavaScript/questao4.js
@@ -1,12 +1,23 @@
 let vetor = [1,2,3,4,5,6,7,8,9,0]
 
-let i = 2;
-if(vetor[i] === 1 || vetor[i] === 2){
-    document.write(vetor[i]);
-}
+let i = 0, j = 2;
 
-for(i = 2; i < vetor.length; i++){
-    if(vetor[i] % i !== 0){
-        console.log(vetor[i]);
+for(i = 0; i < vetor.length; i++){
+    let num = vetor[i];
+    if(num > 0){
+        if(num === 1 || num === 2){
+            document.write(num + " ");
+        }else{
+            let primo = true;
+            for (j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j === 0) {
+                    primo = false;
+                }
+            }
+    
+            if (primo) {
+                document.write(num + " ");
+            }
+        }
     }
 }
